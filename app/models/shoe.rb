399c84167a14e97291ec9cbf571729dc3ed40d60
hai14894 <hohoanghai14894@gmail.com>
2020-05-02 05:22:48 -0700

    more styling-added default picture

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -2,4 +2,10 @@ class Shoe < ApplicationRecord
   enum condition: {brand_new: 1, used: 0}
   belongs_to :location
   belongs_to :user
+  has_one_attached :picture
+
+  def shoe_attachment_path
+    picture.attached? ? picture : "default.jpeg"
+
+  end
 end
